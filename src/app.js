9f6b4fdcb0aedc7442016f9e86b8384717b511a2
Thomas Tyler <thomas.tyler1@aexp.com>
2018-09-10 22:35:34 -0400

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
-import { addExpense } from './actions/expenses'
 import configureStore from './store/configureStore'
 import getVisibleExpenses from './selectors/expenses'
 import { Provider } from 'react-redux';
@@ -11,9 +10,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
 const state = store.getState();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
